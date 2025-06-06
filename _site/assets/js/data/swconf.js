bdd9435d171fde0e7e256fdaf0bfd00841db83c6
Andrew Koren <korenand@msu.edu>
2025-06-06 20:24:02 +0000

    added linux, should make actions work

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749231983',resources: [
+    cacheName: 'chirpy-1749234746',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

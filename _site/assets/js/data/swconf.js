743de67395554dacfe5dbc79deec70c7f1da81f3
Andrew Koren <korenand@msu.edu>
2025-06-08 01:02:07 +0000

    disabled testing

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749234746',resources: [
+    cacheName: 'chirpy-1749344206',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
